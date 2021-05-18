fe7d339c291c744b1f7da082f90d110bd2392f48
João Vinicius Vitral <joaoviniciusvitral@hotmail.com>
2021-05-18 17:58:57 -0300

    Teste 05

---
+++ Prog1-PUCRJ/Teste 05/Teste05.py
@@ -0,0 +1,43 @@
+#    @jxvtrl    #
+
+def replica(s, c = False):
+    v = ("a", "e", "i", "o", "u")
+    p = []
+
+    if c == False:
+        x = len(s)-1
+        Ponto = False
+
+        while x >= 0:
+            if s[x] == ".":
+                Ponto = True
+                p.append(s[x])
+                corta = s[0:x]
+                p = p + replica(corta, True)
+
+                break
+            else:
+                p.append(s[x])
+
+            x = x - 1
+
+        if Ponto:
+            return "".join(p[::-1]) 
+        else:
+            return "String Vazia"
+            
+    else:
+        x = 0
+
+        while x < len(s): 
+            p.append(s[x])
+
+            if s[x] in v:
+                p.append(s[x]*2)
+                
+            x = x + 1
+        return p[::-1]
+
+p = input("Escreva duas palavras separadas por '.': ")
+p = replica(p)
+print("A nova string é: {}".format(p))
